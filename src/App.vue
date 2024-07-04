d4dc1ebd40b9d1579097b25f3d3b4e2c99e2200a
Nicolas A. Juanico <nicolas.juany@gmail.com>
2024-07-05 00:54:01 +0200

    Create and import base components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import BalanceComponent from './components/BalanceComponent.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+export default {
+  components: {
+    HeaderComponent,
+    BalanceComponent,
+    IncomeExpense,
+    TransactionList,
+    AddTransaction
+  }
+}
+
+</script>
 <template>
-  My App
+  <HeaderComponent />
+  <div class="container">
+    <BalanceComponent />
+    <IncomeExpense />
+    <TransactionList />
+    <AddTransaction />
+  </div>
 </template>
